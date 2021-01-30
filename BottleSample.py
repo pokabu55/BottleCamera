15885843e51de5b62f2bb2278fa4296c575a1021
pokabu55 <okazumaru@gmail.com>
2021-01-31 02:36:06 +0900

    サンプル２を追記

--- BottleSample.py
+++ BottleSample.py
@@ -1,7 +1,23 @@
-from bottle import route, run
+from bottle import route, run, template
 
+"""
+# /helloパスをhello()関数（コールバック関数）にリンク
 @route("/hello")
 def hello():
     return "Hello World!"
+
+run(host="localhost", port=8080, debug=True)
+"""
+
+
+"""
+# サンプル２
+1つのコールバックに複数のルートをバインドすることができること
+URLにワイルドカードを追加してキーワード引数を介してアクセスすることができる
+"""
+@route('/')
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
 
 run(host="localhost", port=8080, debug=True)
