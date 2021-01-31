8e204d102ed233c333a9ad1124873c474fef6cbd
pokabu55 <okazumaru@gmail.com>
2021-01-31 23:04:23 +0900

    静的ルーティング

--- BottleSample.py
+++ BottleSample.py
@@ -1,6 +1,7 @@
 from bottle import route, run, template
 
 """
+# サンプル１
 # /helloパスをhello()関数（コールバック関数）にリンク
 @route("/hello")
 def hello():
@@ -14,7 +15,7 @@ run(host="localhost", port=8080, debug=True)
 # サンプル２
 1つのコールバックに複数のルートをバインドすることができること
 URLにワイルドカードを追加してキーワード引数を介してアクセスすることができる
-"""
+
 @route('/')
 @route('/hello/<name>')
 def greet(name='Stranger'):
@@ -23,3 +24,15 @@ def greet(name='Stranger'):
 run(host="localhost", port=8080, debug=True)
 # vaioは固定IPとしたので、外部のクライアントから問い合わせる場合はこちらでrunする
 #run(host="192.168.10.201", port=8080, debug=True)
+"""
+
+"""
+# サンプル３
+静的ルーティング
+"""
+@route('/hello')
+def hello():
+    return template("view/index.tpl")
+
+run(host="localhost", port=8080, debug=True)
+
